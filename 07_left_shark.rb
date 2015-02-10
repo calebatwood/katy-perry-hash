cd18717bb4d3d163806598a52bfde954f0e04c7d
Caleb Atwood <calebatwood@Calebs-MacBook-Air.local>
2015-02-10 16:21:35 -0700

    condense shark code

--- 07_left_shark.rb
+++ 07_left_shark.rb
@@ -3,10 +3,12 @@ require_relative "person"
 # Add Katy's left shark candidates below to the KATY_PERRY hash. Don't edit the perosn.rb file!
 # After you add the shark data, print out the first and last candidate Katy Perry could choose from.
 
-KATY_PERRY[:sharks] = {
+sharks = {
   left_shark: {
     candidates: ["Bobby Wilson", "50cent", "Channing Tatum", "Ron Swanson", "McJagger"]
   }
 }
 
-puts "#{KATY_PERRY[:sharks][:left_shark][:candidates].first} and #{KATY_PERRY[:sharks][:left_shark][:candidates].last}"
+KATY_PERRY.merge!(sharks)
+puts KATY_PERRY[:left_shark][:candidates].first
+puts KATY_PERRY[:left_shark][:candidates].last
